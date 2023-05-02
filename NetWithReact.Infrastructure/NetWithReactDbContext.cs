36bbd4864e3a5a0880087c52fae65240b3e39172
Melisa Şahin <111955439+melisasahinn@users.noreply.github.com>
2023-05-02 22:55:30 +0300

    .

---
+++ NetWithReact.Infrastructure/NetWithReactDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetWithReact.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetWithReact.Infrastructure
+{
+    public class NetWithReactDbContext : DbContext
+    {
+        public const string Connection = "PostgreSQLConnection";
+
+        public NetWithReactDbContext(DbContextOptions<NetWithReactDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<OrderItems> OrderItems { get; set; }
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductInventory> ProductInventories { get; set; }
+        public DbSet<ShoppingSession> ShoppingSessions { get; set; }
+        public DbSet<UserAddress> UserAddresses { get; set; }
+        public DbSet<UserPayment> UserPayments { get; set; }
+
+    }
+}
