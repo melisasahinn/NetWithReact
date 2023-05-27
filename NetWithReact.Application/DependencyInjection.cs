dd7d7c2b2beb57b7d49ddeb880e80b5d27ad53f5
Melisa Şahin <111955439+melisasahinn@users.noreply.github.com>
2023-05-27 18:52:57 +0300

    application added

---
+++ NetWithReact.Application/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NetWithReact.Application.Common.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetWithReact.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services) 
+        {
+            services.AddMediatR(
+                cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly) );
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
--- NetWithReact.Infrastructure/DependencyInjection.cs
+++ NetWithReact.Infrastructure/DependencyInjection.cs
@@ -1,15 +1,39 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using NetWithReact.Application.Common.Interfaces.Persistence;
+using NetWithReact.Infrastructure.Persistence.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
 
 namespace NetWithReact.Infrastructure
 {
     public static class DependencyInjection
     {
-      
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services
+                .AddPersistence(configuration) ;
+            return services ;
+        }
+        private static IServiceCollection AddPersistence(this IServiceCollection services,ConfigurationManager configuration)
+        {
+            services.AddScoped<ICartItemRepository, CartItemRepository>();
+            services.AddScoped<IDiscountRepository,DiscountRepository>();
+            services.AddScoped<IOrderDetailsRepository, OrderDetailRepository>();
+            services.AddScoped<IPaymentDetailsRepository, PaymentDetailRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductInventoryRepository,ProductInventoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IShoppingSessionRepository, ShoppingSessionRepository>();
+            services.AddScoped<IUserAddressRepository, UserAddressRepository>();
+            services.AddScoped<IUserPaymentRepository, UserPaymentRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services ;
+        }
     }
 }
