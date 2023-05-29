a41d4b4c3adac8b0d91ce1a507963eb177707966
Melisa Şahin <111955439+melisasahinn@users.noreply.github.com>
2023-05-29 18:14:14 +0300

    updated

--- NetWithReact.Application/Discounts/Command/CreateDiscount/CreateDiscountCommand.cs
+++ NetWithReact.Application/Discounts/Command/CreateDiscount/CreateDiscountCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ErrorOr;
+using MediatR;
+using NetWithReact.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,6 @@ using System.Threading.Tasks;
 
 namespace NetWithReact.Application.Discounts.Command.CreateDiscount
 {
-    internal class CreateDiscountCommand
-    {
-    }
+    public record CreateDiscountCommand(string Name, decimal DiscountPercent):IRequest<ErrorOr<Discount>>;
+
 }
--- NetWithReact.Application/Discounts/Queries/GetByIdDiscounts/GetByIdDiscountQueryResult.cs
+++ NetWithReact.Application/Discounts/Queries/GetByIdDiscounts/GetByIdDiscountQueryResult.cs
@@ -7,6 +7,5 @@ using System.Threading.Tasks;
 namespace NetWithReact.Application.Discounts.Queries.GetByIdDiscounts
 {
     public record GetByIdDiscountQueryResult(long Id, string Name, decimal DiscountPercent);
-    {
-    }
+   
 }
